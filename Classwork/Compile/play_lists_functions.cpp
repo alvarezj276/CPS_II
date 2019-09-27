78d40bf5f6742242666b6e7e893e441d6d4fc510
mxc6641 <mxc6641@ECTET-1350-20.main.ad.rit.edu>
2019-09-27 11:51:27 -0400

    halfway done

--- Classwork/Compile/play_lists_functions.cpp
+++ Classwork/Compile/play_lists_functions.cpp
@@ -1,5 +1,8 @@
 #include <string>
+#include <iostream>
+#include <string>
 #include <iomanip>
+#include <fstream>
 
 using namespace std;
 
@@ -16,10 +19,13 @@ struct song{
 struct playlist{
 	string name;
 	song item;
-	song nextaddr;
+	playlist nextaddr;
 }
 
-Create(){
+int list;
+playlist list_name[5];
+
+void Create(){
 	char select;
 	if(list >= 5){
 		cout << "Maximum number of playlists in library. Upgrade to premium for additional storage." << endl;
@@ -27,15 +33,34 @@ Create(){
 	}
 	else{
 		list++;
+		int position; char performer; string name;
 		cout << "New Playlist: \nEnter Playlist name: ";
-		cin >> playlist->name;
+		cin >> ;
 		cout << "Choose Playlist entry-maker: \n  (R)anking     (D)ecade     (P)erformer     (G)enre";
 		cin >> select; cin.ignore();
 		do{
+			switch(select){
+			case 'r':
+			case 'R':cout << "Enter song ranking: ";
+					 cin >> position;
 
+				break;
+			case 'd':+
+			case 'D':cout << "Enter song decade: ";
+					 cin >> position;
+				break;
+			case 'p':
+			case 'P':cout << "Enter song performer: ";
+					 cin >> performer;
+				break;
+			case 'g':
+			case 'G':cout << "|1| Latin\n|2| Country\n|3| Hip-Hop/Rap\n|4| Jazz\n|5| Dance/Electronic\n|6| R&B\n|7| Pop\n|8| Rock\nEnter song genre number: ";
+					 cin >> position;
+				break;
+			}
 		}while(select == 'R' || select == 'r'
 				|| select == 'D' || select == 'd'
-				|| select == 'P' || select == 'f'
-				|| select == 'G' || select == 'g')
+				|| select == 'P' || select == 'p'
+				|| select == 'G' || select == 'g');
 	}
 }
