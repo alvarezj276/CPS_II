52fc50e77d18bc11f47633c4910a7482bd40a096
mxc6641 <mxc6641@ECTET-1350-20.main.ad.rit.edu>
2019-10-02 11:50:56 -0400

    up to decade: remember, there is a DECADE in the song

--- Classwork/Compile/play_lists_functions.cpp
+++ Classwork/Compile/play_lists_functions.cpp
@@ -14,26 +14,24 @@ struct song{
 	int decade;
 	char performer;
 	int genre;
+	song *nextaddr;
 }
 
 struct playlist{
 	string name;
 	song item;
-	playlist nextaddr;
+	playlist *nextaddr;
 }
-
-int list;
-playlist list_name[5];
-
 int getCount(playlist *head)
 {
     if (head == NULL)
         return 0;
     return getCount(head->nextaddr)+1;
-
 }
 
 void Create(){
+	int list;
+	playlist list_name[5];
 	char select;
 	if(list >= 5){
 		cout << "Maximum number of playlists in library. Upgrade to premium for additional storage." << endl;
@@ -41,12 +39,21 @@ void Create(){
 	}
 	else{
 		list++;
-		int position; char performer; string name;
+		int position; char checker; string name;
 		playlist *current,*head,*next;
 		current = new playlist;
+		int year=1950;
+		int decade[7][10];
+		for(int i=0;i<7;i++){
+			for(int k=0;k<10;k++){
+				decade[i][k]=year+k;
+			}
+			year+=10;
+		}
 		cout << "New Playlist: \nEnter Playlist name: ";
 		getline(cin,current->name);
 		list_name[list]=current;
+		head=current;
 		do{
 			cout << "Choose Playlist entry-maker: \n  (R)anking     (D)ecade     (P)erformer     (G)enre";
 			cin >> select; cin.ignore();
@@ -59,26 +66,46 @@ void Create(){
 						break;
 					}
 					while(true){
-						song *tmpcurrent,*tmpprevious,*tmp;
+						song *tmpcurrent;
 						if(tmpcurrent->rank == position){
-							if(current)
+							if(getCount(head)>1)
+								next = new playlist;
 							current->item=tmpcurrent;
-							current=current->nextaddr;
+							next=current->nextaddr;
+							break;
 						}
-
+						tmpcurrent=tmpcurrent->nextaddr;
 					}
 					break;
-			case 'd':+
+			case 'd':
 			case 'D':cout << "Enter song decade: ";
-					 cin >> position;
-				break;
+					cin >> name;
+					if(name != "50" || name != "60" || name != "70" || name != "80"
+							 || name != "90" || name != "00" || name != "10"){
+						cout << "Decade not valid. Reselect entry-maker" << endl;
+						break;
+					}
+					while(true){
+						song *tmpcurrent,*tmpprevious,*tmp;
+
+						tmpcurrent=tmpcurrent->nextaddr;
+						if(tmpcurrent->decade == position){
+							if(getCount(head)>1)
+								next = new playlist;
+							current->item=tmpcurrent;
+							next=current->nextaddr;
+							break;
+						}
+						tmpcurrent=tmpcurrent->nextaddr;
+					}
+					break;
 			case 'p':
 			case 'P':cout << "Enter song performer: ";
-					 cin >> performer;
+					 cin >> checker;
 				break;
 			case 'g':
 			case 'G':cout << "|1| Latin\n|2| Country\n|3| Hip-Hop/Rap\n|4| Jazz\n|5| Dance/Electronic\n|6| R&B\n|7| Pop\n|8| Rock\nEnter song genre number: ";
-					 cin >> position;
+					 cin >> checker;
 				break;
 			}
 		}while(select == 'R' || select == 'r'
