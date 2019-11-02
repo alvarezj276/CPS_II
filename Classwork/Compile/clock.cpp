3cad50372d73bc01f226c9d115db93b4eb8c2285
mxc6641 <mxc6641@ECTET-1350-20.main.ad.rit.edu>
2019-11-02 12:01:56 -0400

    clock needs help

--- Classwork/Compile/clock.cpp
+++ Classwork/Compile/clock.cpp
@@ -21,6 +21,16 @@ private:
 	int second;
 public:
 	clock_18(int h=0,int m=0,int s=0){
+		if(s>59){
+			s=0;
+			m++;
+		}
+		if(m>59){
+			m=0;
+			h++;
+		}
+		if(h>17)
+			h=0;
 		hour=h;
 		minute=m;
 		second=s;
@@ -30,7 +40,7 @@ public:
 		minute=m;
 		second=s;
 	};
-	friend istream& operator>>(istream& time_in, const clock_18& X){
+	friend istream& operator>>(istream& time_in, clock_18& X){
 		cin >> X.hour;
 		cin >> X.minute;
 		cin >> X.second;
@@ -48,14 +58,14 @@ public:
 		if(second>59){
 			second=0;
 			minute++;
-			if(minute>59){
-				minute=0;
-				hour++;
-				if(hour>17){
-					hour=0;
-					second++;
-				}
-			}
+		}
+		if(minute>59){
+			minute=0;
+			hour++;
+		}
+		if(hour>17){
+			hour=0;
+			second++;
 		}
 		return *this;
 	};
@@ -89,6 +99,18 @@ public:
 		hour=X/3600;
 		minute=(X-(hour*3600))/60;
 		second=X-(hour*3600)-(minute*60);
+		if(second>59){
+			second-=60;
+			minute++;
+		}
+		if(minute>59){
+			minute=0;
+			hour++;
+		}
+		if(hour>17){
+			hour=0;
+			second++;
+		}
 	};
 	operator clock_12();
 };
@@ -96,23 +118,30 @@ public:
 class clock_12{
 private:
 	int hour,minute,second;
+	char meridiem;
 public:
-	clock_12(int h=0,int m=0,int s=0){
+	clock_12(int h=0,int m=0,int s=0, char t='a'){
 		hour=h;
 		minute=m;
 		second=s;
+		meridiem=t;
 	};
-	friend istream& operator>>(istream& time_in, const clock_12& X){
+	friend istream& operator>>(istream& time_in, clock_12& X){
 		cin >> X.hour;
 		cin >> X.minute;
 		cin >> X.second;
+		cin >> X.meridiem;
 		return time_in;
 	};
 	friend ostream& operator<<(ostream& time_out, const clock_12& X){
 		cout << setfill('0');
 		cout << setw(2) << X.hour << ':';
 		cout << setw(2) << X.minute << ':';
-		cout << setw(2) << X.second << ':';
+		cout << setw(2) << X.second << ' ';
+		if(X.meridiem=='a')
+			cout << setw(2) << "AM" << endl;
+		else
+			cout << setw(2) << "PM" << endl;
 		return time_out;
 	};
 	operator clock_18();
@@ -122,17 +151,71 @@ clock_18::operator clock_12(){
 	int h=hour;
 	int m=minute;
 	int s=second;
-	return clock_12(h,m,s);
+	char t='a';
+	if(hour >= 12){
+		t='p';
+		if(hour>12)
+			h=hour-12;
+	}
+	return (clock_12(h,m,s,t));
 };
 
 clock_12::operator clock_18(){
 	int h=hour;
 	int m=minute;
 	int s=second;
-	return clock_18(h,m,s);
+	if(meridiem=='p')
+		if(hour > 12)
+			h=hour+12;
+	return (clock_18(h,m,s));
 };
 
 int main(){
-	clock_18 startTime,endTime,watchStart,watchEnd;
+	long t,start,end;
+	clock_18 startTime,endTime,watchStart,watchEnd,timer;
 	clock_12 watchStart_12, watchEnd_12;
+	bool clock=true;
+	do{
+		cout << "Enter start time: ";
+		cin >> startTime;
+		cout << "Enter end time: ";
+		cin >> endTime;
+		cout << "Enter starting watch: ";
+		cin >> watchStart;
+		cout << "Enter ending watch: ";
+		cin >> watchEnd;
+		if((long(endTime) < long(startTime)) && (long(watchStart) < long(watchEnd))
+				&& (long(watchStart) > long(startTime)) && (long(watchStart) < long(endTime))
+				&& (long(watchEnd) < long(endTime)) && (long(watchEnd) > long(startTime))){
+			do{
+				start=long(startTime);
+				end=long(endTime);
+				if(start < long(watchStart))
+					cout << "Sleeping | ";
+				if((start >= long(watchStart)) && (start<= watchEnd))
+					cout << "On Watch | ";
+				if(start > long(watchEnd))
+					cout << "Off-Duty | ";
+				cout << startTime;
+				++startTime;
+			}while(startTime!=endTime);
+			clock=false;
+		}
+		else
+			cout << "Time incorrect, please re-enter values" << endl;
+	}while(clock);
+	watchStart_12 = clock_12(watchStart);
+	watchEnd_12 = clock_12(watchEnd);
+	cout << "Summary:\nWatch Start (18) | ";
+	cout << watchStart;
+	cout << "Watch End(18) | ";
+	cout << watchEnd;
+	cout << "Watch Start (12) | ";
+	cout << watchStart_12;
+	cout << "Watch End(12) | ";
+	cout << watchEnd_12;
+	t=long(watchEnd)-long(watchStart);
+	clock_18 finalTime(t);
+	cout << "Total time | ";
+	cout << finalTime;
 }
