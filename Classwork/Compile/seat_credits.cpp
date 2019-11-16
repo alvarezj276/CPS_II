bdbad4b4681c67515cdfe5a00b6d11045778b84b
mxc6641 <mxc6641@ECTET-1350-20.main.ad.rit.edu>
2019-11-16 16:43:52 -0500

    create finished?

--- Classwork/Compile/seat_credits.cpp
+++ Classwork/Compile/seat_credits.cpp
@@ -112,7 +112,7 @@ public:
 	}
 };
 
-void Create(passenger *head){
+void Create(passenger *head, Pickup pick[3], Compact comp[3], Sedan sed[3]){
 	passenger *current=head;
 	string first,last;
 	bool quit=false;
@@ -121,7 +121,8 @@ void Create(passenger *head){
 		cin >> first; cin >> last;
 		bool namecheck=false;
 		passenger *current;
-		int type, vehicle,seat;
+		string type, color;
+		int seat;
 		char select=' ';
 		while(current != NULL){
 			if(first==current->fname && last==current->lname){
@@ -137,17 +138,311 @@ void Create(passenger *head){
 			quit=true;
 		else{
 //			Print();
-			cout << "Select by category? (Y/N)";
+			cout << "(V)ehicle or (S)pecific seating: ";
 			cin >> select; cin.ignore();
-			if(select=='Y' || select=='y'){
-				cout << "Select vehicle type (1-3): ";
-				cin >> type; cin.ignore();
-				if(type==1){
-
+			bool space=false;
+			if(select=='V' || select=='v'){
+				cout << "Select vehicle type: ";
+				cin >> type;
+				if(type=="TRUCK" || type=="Truck" || type=="truck"){
+					if(current->credit<5){
+						cout<<"Not enough credits. Please reselect." << endl;
+						space=true;
+					}
+					else
+						for(int i=0;i<3;i++){
+							if(pick[i].front!=0){
+								pick[i].front=0;
+								current->credit-=5;
+								space=true;
+								break;
+							}
+						}
 				}
+				else{
+					if(type=="COMPACT" || type=="Compact" || type=="compact"){
+						if(current->credit<3){
+							cout<<"Not enough credits. Please reselect." << endl;
+							space=true;
+						}
+						else
+							for(int i=0;i<3;i++){
+								if(comp[i].front!=0){
+									comp[i].front=0;
+									if(current->credit<5)
+										cout << "Not enough credits. Please reselect." << endl;
+									else
+										current->credit-=5;
+									space=true;
+									break;
+								}
+								if(comp[i].back1!=0){
+									comp[i].back1=0;
+									current->credit-=3;
+									space=true;
+									break;
+								}
+								if(comp[i].back2!=0){
+									comp[i].back2=0;
+									current->credit-=3;
+									space=true;
+									break;
+								}
+							}
+					}
+					else{
+						if(type=="SEDAN" || type=="Sedan" || type=="sedan"){
+							if(current->credit<1){
+								cout<<"Not enough credits. Please reselect." << endl;
+								space=true;
+							}
+							else
+								for(int i=0;i<3;i++){
+									if(sed[i].front!=0){
+										sed[i].front=0;
+										if(current->credit<5)
+											cout << "Not enough credits. Please reselect." << endl;
+										else
+											current->credit-=5;
+										space=true;
+										break;
+									}
+									if(sed[i].sideback1!=0){
+										sed[i].sideback1=0;
+										if(current->credit<2)
+											cout << "Not enough credits. Please reselect." << endl;
+										else
+											current->credit-=2;
+										space=true;
+										break;
+									}
+									if(sed[i].sideback2!=0){
+										sed[i].sideback2=0;
+										if(current->credit<2)
+											cout << "Not enough credits. Please reselect." << endl;
+										else
+											current->credit-=2;
+										space=true;
+										break;
+									}
+									if(sed[i].sidemiddle!=0){
+										sed[i].sidemiddle=0;
+										current->credit-=1;
+										space=true;
+										break;
+									}
+								}
+						}
+					}
+				}
+				if(space==false)
+					cout << "No room available in this vehicle type. Please reselect." << endl;
+				else
+					quit=true;
 			}
-			if(select=='N' || select=='n'){
+			if(select=='S' || select=='s'){
+				cout << "Select vehicle type: ";
+				cin >> type;
+				if(type=="TRUCK" || type=="Truck" || type=="truck"){
+//					Print();
+					if(current->credit<5){
+						cout<<"Not enough credits. Please reselect." << endl;
+						space=true;
+					}
+					for(int i=0;i<3;i++)
+						if(pick[i].front!=0)
+							space=true;
+					if(space==false)
+						cout<<"Not enough room. Please select another vehicle type." << endl;
+					else{
+						cout << "Select color of vehicle: ";
+						cin>>color;
+						if(color=="PURPLE" || color=="Purple" || color=="purple"){
+							pick[0].front=0;
+							current->credit-=5;
+						}
+						if(color=="YELLOW" || color=="Yellow" || color=="yellow"){
+							pick[1].front=0;
+							current->credit-=5;
+						}
+						if(color=="RED" || color=="Red" || color=="red"){
+							pick[2].front=0;
+							current->credit-=5;
+						}
+						else
+							cout << "Color not found. Please reselect." << endl;
+					}
+				}
+				else{
+					if(type=="COMPACT" || type=="Compact" || type=="compact"){
+						//						Print();
+						if(current->credit<3){
+							cout<<"Not enough credits. Please reselect." << endl;
+							space=true;
+						}
+						for(int i=0;i<3;i++)
+							if(comp[i].front!=0 || comp[i].back1!=0 || comp[i].back2!=0)
+								space=true;
+						if(space==false)
+							cout<<"Not enough room. Please select another vehicle type." << endl;
+						else{
+							cout << "Select color of vehicle: ";
+							cin>>color;
+							if(color=="GREEN" || color=="Green" || color=="green"){
+								cout << "Select seat (0-2): ";
+								cin >> seat; cin.ignore();
+								if(seat==0 || seat==1 || seat==2){
+									if(seat==0){
+										comp[0].front=0;
+										current->credit-=5;
+									}
+									if(seat==1){
+										comp[0].back1=0;
+										current->credit-=3;
+									}
+									if(seat==2){
+										comp[0].back2=0;
+										current->credit-=3;
+									}
+								}
+								else
+									cout << "Seat not found. Please reselect." << endl;
+							}
+							if(color=="BLUE" || color=="Blue" || color=="blue"){
+								cout << "Select seat (0-2): ";
+								cin >> seat; cin.ignore();
+								if(seat==0 || seat==1 || seat==2){
+									if(seat==0){
+										comp[1].front=0;
+										current->credit-=5;
+									}
+									if(seat==1){
+										comp[1].back1=0;
+										current->credit-=3;
+									}
+									if(seat==2){
+										comp[1].back2=0;
+										current->credit-=3;
+									}
+								}
+								else
+									cout << "Seat not found. Please reselect." << endl;
+							}
+							if(color=="YELLOW" || color=="Yellow" || color=="yellow"){
+								cout << "Select seat (0-2): ";
+								cin >> seat; cin.ignore();
+								if(seat==0 || seat==1 || seat==2){
+									if(seat==0){
+										comp[2].front=0;
+										current->credit-=5;
+									}
+									if(seat==1){
+										comp[2].back1=0;
+										current->credit-=3;
+									}
+									if(seat==2){
+										comp[2].back2=0;
+										current->credit-=3;
+									}
+								}
+								else
+									cout << "Seat not found. Please reselect." << endl;
+							}
+						}
+					}
+					else{
+						if(type=="SEDAN" || type=="Sedan" || type=="sedan"){
+							//							Print();
+							if(current->credit<1){
+								cout<<"Not enough credits. Please reselect." << endl;
+								space=true;
+							}
+							for(int i=0;i<3;i++)
+								if(sed[i].front!=0 || sed[i].sideback1!=0 || sed[i].sideback2!=0 || sed[i].sidemiddle!=0)
+									space=true;
+							if(space==false)
+								cout<<"Not enough room. Please select another vehicle type." << endl;
+							else{
+								cout << "Select color of vehicle: ";
+								cin>>color;
+								if(color=="RED" || color=="Red" || color=="red"){
+									cout << "Select seat (0-3): ";
+									cin >> seat; cin.ignore();
+									if(seat==0 || seat==1 || seat==2 || seat==3){
+										if(seat==0){
+											sed[0].front=0;
+											current->credit-=5;
+										}
+										if(seat==1){
+											sed[0].sideback1=0;
+											current->credit-=2;
+										}
+										if(seat==2){
+											sed[0].sideback2=0;
+											current->credit-=2;
+										}
+										if(seat==3){
+											sed[0].sidemiddle=0;
+											current->credit-=1;
+										}
+									}
+									else
+										cout << "Seat not found. Please reselect." << endl;
+								}
+								if(color=="GREEN" || color=="Green" || color=="green"){
+									cout << "Select seat (0-3): ";
+									cin >> seat; cin.ignore();
+									if(seat==0 || seat==1 || seat==2 || seat==3){
+										if(seat==0){
+											sed[1].front=0;
+											current->credit-=5;
+										}
+										if(seat==1){
+											sed[1].sideback1=0;
+											current->credit-=2;
+										}
+										if(seat==2){
+											sed[1].sideback2=0;
+											current->credit-=2;
+										}
+										if(seat==3){
+											sed[1].sidemiddle=0;
+											current->credit-=1;
+										}
+									}
+									else
+										cout << "Seat not found. Please reselect." << endl;
+								}
+								if(color=="BLUE" || color=="Blue" || color=="blue"){
+									cout << "Select seat (0-3): ";
+									cin >> seat; cin.ignore();
+									if(seat==0 || seat==1 || seat==2 || seat==3){
+										if(seat==0){
+											sed[2].front=0;
+											current->credit-=5;
+										}
+										if(seat==1){
+											sed[2].sideback1=0;
+											current->credit-=2;
+										}
+										if(seat==2){
+											sed[2].sideback2=0;
+											current->credit-=2;
+										}
+										if(seat==3){
+											sed[2].sidemiddle=0;
+											current->credit-=1;
+										}
+									}
+									else
+										cout << "Seat not found. Please reselect." << endl;
+								}
 
+							}
+							if(space==false)
+								cout << "No room available in this vehicle type. Please reselect." << endl;
+							else
+								quit=true;
 			}
 			else{
 				cout << "Incorrect vehicle type. Please restart selection." << endl;
@@ -206,9 +501,12 @@ int main()
 	pick[2].setCar("RED","TRUCK");
 	Compact comp[3];
 	comp[0].setCar("GREEN","COMPACT");
-	comp[1].setCar("GREEN","COMPACT");
-	comp[2].setCar("GREEN","COMPACT");
+	comp[1].setCar("BLUE","COMPACT");
+	comp[2].setCar("YELLOW","COMPACT");
 	Sedan sed[3];
+	sed[0].setCar("RED","SEDAN");
+	sed[1].setCar("GREEN","SEDAN");
+	sed[2].setCar("BLUE","SEDAN");
 
 	passenger *head = Read();
 	cout << head->fname << endl;
